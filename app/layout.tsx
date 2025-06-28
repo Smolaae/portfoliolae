52ad0f93ab2569e0843e6f1d1aac4fc06abcfc41
Smolae <laetitiados_santos@hotmail.com>
2025-06-28 23:23:20 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </head>
       <body className={inter.className}>{children}
       </body>
