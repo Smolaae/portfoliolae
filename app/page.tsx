2f227d3e77c4ea4b41ecd8bf28a694f9cbdef2ab
Smolae <laetitiados_santos@hotmail.com>
2025-06-28 22:25:47 +0200

    Vercel package

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import About from "@/components/About"
 import Projects from "@/components/Projects"
 import Contact from "@/components/Contact"
 import { ScrollProgress } from "@/components/ScrollAnimations"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main>
+      <SpeedInsights />
       <ScrollProgress />
       <Hero />
       <About />
